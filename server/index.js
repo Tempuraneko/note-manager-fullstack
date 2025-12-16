c31a05d6c9642f7f9fd346560c07aeda03c9b507
Tempuraneko <ngjiawei0201@gmail.com>
2025-12-17 02:33:39 +0800

    Fix Railway sqlite issue by removing node_modules

--- server/index.js
+++ server/index.js
@@ -121,6 +121,7 @@ app.delete('/api/notes/:id', (req, res) => {
     });
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
